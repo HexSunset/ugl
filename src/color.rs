80eea6f22132014fd8da7f6b4193e5e29d3eec1e
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2023-03-13 13:10:17 +0200

    Parse color from u32, convenience functions

--- src/color.rs
+++ src/color.rs
@@ -1,4 +1,4 @@
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Color {
     r: u8,
     g: u8,
@@ -11,19 +11,54 @@ impl Color {
         Self { r, g, b, a }
     }
 
-    pub fn red(&self) -> u8 {
+    pub fn red(&self) -> f64 {
+        self.r as f64 / 255.0 as f64
+    }
+
+    pub fn green(&self) -> f64 {
+        self.g as f64 / 255.0 as f64
+    }
+
+    pub fn blue(&self) -> f64 {
+        self.b as f64 / 255.0 as f64
+    }
+
+    pub fn alpha(&self) -> f64 {
+        self.a as f64 / 255.0 as f64
+    }
+
+    pub fn red_u8(&self) -> u8 {
         self.r
     }
 
-    pub fn green(&self) -> u8 {
+    pub fn green_u8(&self) -> u8 {
         self.g
     }
 
-    pub fn blue(&self) -> u8 {
+    pub fn blue_u8(&self) -> u8 {
         self.b
     }
 
-    pub fn alpha(&self) -> u8 {
+    pub fn alpha_u8(&self) -> u8 {
         self.a
+    }
+
+    pub fn from_hex(x: u32) -> Color {
+        let r: u8 = ((x & 0xFF000000) >> 8 * 3).try_into().unwrap();
+        let g: u8 = ((x & 0x00FF0000) >> 8 * 2).try_into().unwrap();
+        let b: u8 = ((x & 0x0000FF00) >> 8 * 1).try_into().unwrap();
+        let a: u8 = ((x & 0x000000FF) >> 8 * 0).try_into().unwrap();
+
+        Color { r, g, b, a }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn color_from_u32() {
+        assert_eq!(Color::from_hex(0xFFFFFFFF), Color::new(255, 255, 255, 255));
     }
 }
